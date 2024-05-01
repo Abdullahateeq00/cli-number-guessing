2f5c9979b06a9fa5de953443fe838d05865dc045
Abdullah <abdullahateeq331@gmail.com>
2024-05-01 02:57:03 -0700

    commits
    this is a number guessing game based on cli project

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a number:"
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("CONGRATULATIONS! YOU WIN");
+}
+else {
+    console.log(`YOU GUESSED WRONG NUMBER, YOUR RIGHT NUMBER IS: ${randomNumber}`);
+}
