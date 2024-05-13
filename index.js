4d604b87749ae99ebbd52dea318d88c09f4279f7
Abdullah <abdullahateeq331@gmail.com>
2024-05-12 22:42:05 -0700

    created number-guessing

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+function rainbowText(text) {
+    const rainbowColors = [chalk.red, chalk.yellow, chalk.green, chalk.blue, chalk.magenta];
+    let rainbowText = '';
+    for (let i = 0; i < text.length; i++) {
+        const color = rainbowColors[i % rainbowColors.length];
+        rainbowText += color(text[i]);
+    }
+    return rainbowText;
+}
+console.log(rainbowText('Welcome to number guessing game'));
+const randomNumber = (Math.floor(Math.random() * 5 + 1));
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: chalk.bold("Please Guess a Number (1-5):"),
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log(chalk.yellow("Congratulations! You win"));
+}
+else {
+    console.log(chalk.red("You guessed wrong number " + ",", "your right number is: " +
+        chalk.yellow(`${randomNumber}`)));
+}
